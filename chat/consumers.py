b778ca9ce6772f3e95c5e5146135aecfdc18b941
Hosea Ochieng <OchiengHosea>
2021-08-16 09:25:36 +0300

    Asysnchronous chat

---
+++ chat/consumers.py
@@ -0,0 +1,42 @@
+import json
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # join group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # leave group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        # receive message from web socket
+
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        # send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
---
+++ livechats/asgi.py
@@ -0,0 +1,28 @@
+"""
+ASGI config for livechats project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+from channels.auth import AuthMiddlewareStack
+from channels.http import AsgiHandler
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import chat.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livechats.settings')
+
+application = ProtocolTypeRouter({
+        "http":get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        ) 
+    }
+    # {"http": AsgiHandler(),} # for django 2.2 and below
+)
